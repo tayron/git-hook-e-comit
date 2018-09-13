e4bee9bb465620e3f3ae8c35f0660a1c2dab1637
Tayron Miranda <desenvolvimento.potestatem1@gmail.com>
2018-09-13 16:41:56 -0300

    Corrigindo script do cliente

--- cliente/comitar_enviar_homologacao.sh
+++ cliente/comitar_enviar_homologacao.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "1) Informe a mensagem do commit: "
-echo 
 read MENSAGEM
 
 git add --all
-git commit -m $MENSAGEM
+git commit -m "$MENSAGEM"
 git push -u origin homologacao
 git push deploy_homologacao homologacao
+
